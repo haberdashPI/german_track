a987c6af813573d5dcb6b9e9fa971bcbdb0048b6
David Little <david.frank.little@gmail.com>
2019-03-01 17:21:37 -0500

    Thoughts about analysis approach.

--- analyses/test_object_left.m
+++ analyses/test_object_left.m
@@ -1,6 +1,10 @@
 % First sanity check: can we more accurately recover sources in the right ear
 % when listeners are instructed to attend to the right ear.
 
+% wait a minute: how does this actually work,
+% do we follow a single source, or just all source for a given ear?
+% what is the decoder looking at? a mixture of the sources?
+
 eeg_files = dir(fullfile(data_dir,'eeg_response*.mat'));
 
 [eeg,events,sid] = load_subject(eeg_files(8).name);
