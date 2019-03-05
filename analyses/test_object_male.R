40e161c161a606e8df6bfea1a23a88d0d87ad05e
David Little <david.frank.little@gmail.com>
2019-03-05 09:10:19 -0500

    saved plot of sanity check

--- analyses/test_object_male.R
+++ analyses/test_object_male.R
@@ -1,6 +1,9 @@
 
 source('util/setup.R')
 
+dir = file.path(plot_dir,paste('run',Sys.Date(),sep='_'))
+dir.exists(dir) || dir.create(dir)
+
 df = gather(read.csv(file.path(cache_dir,'testobj.csv')),
             label,cor,male_C:fem2_C)
 
@@ -8,3 +11,5 @@ ggplot(df,aes(x=label,y=cor,color=label)) +
   geom_point(position=position_jitter(width=0.2),color='black',alpha=0.2) +
   stat_summary(geom='pointrange',fun.data='mean_cl_boot') +
   facet_wrap(~sid)
+
+ggsave(file.path(dir,'object_condition.pdf'))
