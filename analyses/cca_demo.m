6a52c32e256218cfd8cea30efdebe23aa0541736
David Little <david.frank.little@gmail.com>
2019-04-17 14:16:49 -0400

    plans for CCA

---
+++ analyses/cca_demo.m
@@ -0,0 +1,19 @@
+x = x - repmat(mean(x,1), size(x,1),1); % subtract mean from each column
+C = x'*x; % covariance matrix
+
+% assuming: same number of channels
+[A,score,AA] = nt_mcca(C,nchan);
+% score: ~variance acounted for components (slow slope might mean more cleaning needs to happen)
+
+comp = X * A; % MCCA components, where first column is most repeatable component across subjects
+nkeep = 20; % number of components to keep
+
+% Project out all but first "nkeep" components
+for i = 1:nsub
+    arr = <subject data in "tall" form, dim= ntime*ntrial x nchan>
+    iA = AA{i}; % subject-specific MCCA weights
+    eye_select = zeros(size(iA,2),1);
+    eye_select(1:nkeep) = 1;
+    y = arr* (iA*diag(eye_select)*pinv(iA));
+    % y: MCCA-cleaned data for subject i
+end
