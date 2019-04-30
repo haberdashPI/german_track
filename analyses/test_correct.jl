03e062121746e7a107911eaebd142eca31fe22bf
David Little <david.frank.little@gmail.com>
2019-04-30 15:14:19 -0400

    test correct with MCCA

--- analyses/test_correct.jl
+++ analyses/test_correct.jl
@@ -3,12 +3,12 @@ include(joinpath(@__DIR__,"..","util","setup.jl"))
 # - train at correct targets
 
 stim_info = JSON.parsefile(joinpath(stimulus_dir,"config.json"))
-eeg_files = filter(x -> occursin(r"_cleaned\.mat$",x),readdir(data_dir))
+eeg_files = filter(x -> occursin(r"_mcca05\.mat$",x),readdir(data_dir))
 
 df = trf_train_speakers("",eeg_files,stim_info,
-    train = "clean_correct" =>
+    train = "mcca05_correct" =>
         row -> row.correct ? all_indices : no_indices,
-    test = "clean_correct" => all_indices,
+    test = "mcca05_correct" => all_indices,
     skip_bad_trials = true)
 
 save(joinpath(cache_dir,"test_correct.csv"),df)
