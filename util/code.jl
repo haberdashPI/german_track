62d0c2c91bc851601abba5baef241689e9f3e71e
David Little <david.frank.little@gmail.com>
2019-05-15 10:55:28 -0400

    WIP: full implementation; test on example unfinished

--- util/code.jl
+++ util/code.jl
@@ -10,7 +10,7 @@ using Unitful
 """
     code(y,X,[state];params...)
 
-Solve single step of online encoding or decoding problem. For decoding y =
+Solve single step of online encoding or decoding problem. For decoding, y =
 speech stimulus, and X = eeg data. For encoding, y = eeg data, X = speech
 stimulus. You should pass a single window of data to the function, with rows
 representing time slices, and columns representing channels (for eeg data) or
@@ -26,16 +26,19 @@ problem
 ```
 
 In the above equation, y is the output, X the input, and θ the parameters to
-be solved for
+be solved for.
 
-Returns an internal state and a solution
+Returns an `Objective` object (see below).
 """
 function code; end # see full defintion below
 
 # defines the objective of optimization
-struct Objective <: ProximableFunction
+struct Objective <: ProximalOperators.Quadratic
+    # sum(i -> λ^i*X[i]'X[i],1:t), where X[i] is input at step i
     A::Matrix{Float64}
+    # sum(i -> λ^i*y[i]'X[i],1:t), where y[i] is output at step i
     b::Matrix{Float64}
+    # model solution
     θ::Matrix{Float64}
     function Objective(y::Union{Vector,Matrix},X::Matrix)
         new(
@@ -45,13 +48,7 @@ struct Objective <: ProximableFunction
         )
     end
 end
-ProximalOperators.is_convex(::Objective) = true
-ProximalOperators.is_smooth(::Objective) = true
-ProximalOperators.is_separable(::Objective) = true
-ProximalOperators.is_quadratic(::Objective) = true
-ProximalOperators.is_strongly_convex(::Objective) = true
-ProximalOperators.fun_name(f::Objective) = "General Quadratic"
-ProximalOperators.fun_dom(f::Objective) = "AbstractArray{Real}, AbstractArray{Real}"
+ProximalOperators.fun_dom(f::Objective) = "AbstractArray{Real}"
 ProximalOperators.fun_expr(f::Objective) = "x ↦ x'Ax - 2bx"
 ProximalOperators.fun_params(f::Objective) = "" # parameters will be too large...
 
@@ -62,30 +59,24 @@ function update!(f::Objective,y,X,λ)
 end
 
 function (f::Objective)(θ)
-    A,b = f.A,f.b
-    y = θ'A*θ; y -= 2 .* b*θ
+    y = θ'f.A*θ; y .-= 2.0*f.b.*θ
     sum(y)
 end
 
 function ProximalOperators.gradient!(y::AbstractArray,f::Objective,x::AbstractArray)
-    A,b = f.A,f.b
-    y .= 2 .* A*x .- 2 .* b'
+    y .= 2.0.*f.A*x .- 2.*f.b'
     f(x)
 end
 
-function code(y,X,state=nothing;λ=0.9,γ=1e-3,kwds...)
+function code(y,X,state=nothing;λ=(1-1/30),γ=1e-3,kwds...)
     state = isnothing(state) ? Objective(y,X) : state
     params = merge((maxit=1000, tol=1e-3, fast=true),kwds)
 
     update!(state,y,X,λ)
-    state.θ = ProximalAlgorithms.FBS(state.θ,fs=state,fq=NormL1(γ);
-        params...)
+    state.θ .= ProximalAlgorithms.FBS(state.θ,fs=state,fq=NormL1(γ);params...)
 
     state
 end
-
-asseconds(x) = x
-asseconds(x::Quantity) = ustrip(uconvert(s,x))
 
 ################################################################################
 """
@@ -94,6 +85,12 @@ asseconds(x::Quantity) = ustrip(uconvert(s,x))
 Computes an attentional marker for each specified target, using the L1 norm
 of the online decoding coefficients.
 """
+function marker; end
+
+asframes(x,signal) = asseconds(x)*samplerate(signal)
+asseconds(x) = x
+asseconds(x::Quantity) = ustrip(uconvert(s,x))
+
 function marker(eeg,targets...;
     # marker parameters
     window=250ms,
@@ -106,11 +103,11 @@ function marker(eeg,targets...;
     # think that's for compensating for something we're not doing here
 
     nt = length(targets)
-    nlag = floor(Int,asseconds(lag)*samplerate(eeg))+1
+    nlag = floor(Int,asframes(lag,eeg))+1
     ntime = size(eeg,1)-L+1
-    window_len = floor(Int,asseconds(window)*samplerate(eeg))
+    window_len = floor(Int,asframes(window,eeg))
     nwindows = div(ntime/window_len)
-    λ = 1 - window_len/(asseconds(estimation_length)*samplerate(eeg))
+    λ = 1 - window_len/(asframes(estimation_length,eeg))
 
     results = map(_ -> Array{Float64}(undef,nwindows),targets)
 
@@ -133,14 +130,11 @@ function marker(eeg,targets...;
 end
 
 ################################################################################
-
-sig(x) = 1/(1+exp(-x))
-
 """
     attention(x,y)
 
 Given two attention markers, x and y, use a batch, probabilistic state space
-module to compute smoothed markers.
+model to compute a smoothed attentional state for x.
 """
 function attention(x1,x2;
     outer_EM_batch = 20,
@@ -153,24 +147,18 @@ function attention(x1,x2;
     a₀ = 2+μ_p^2/σ_p,
     b₀ = μ_p*(a₀-1),
 
-    # TODO: add parameter comments
-    ρ_d₀ = [16.3344,8.2734],
+    # prior of attended (i = 1) and unattended (i = 2) sources
     μ_d₀ = [0.88897,0.16195],
-    α₀ = [5.7111e+02;1.7725e+03],
-    β₀ = [34.96324;2.1424e+02],
-    μ₀ = [0.88897;0.1619])
+    ρ_d₀ = [16.3344,8.2734],
+    α₀ = [5.7111e+02,1.7725e+03],
+    β₀ = [34.96324,2.1424e+02],
+    μ₀ = [0.88897,0.1619])
 
     n = size(x1,1)
 
     # Kalman filtering and smoothing variables
-    z_k_k = zeros(n+1,1);
-    z_k_k_1 = zeros(n+1,1);
-    sig_k_k = zeros(n+1,1);
-    sig_k_k_1 = zeros(n+1,1);
-
-    z_k_K = zeros(n+1,1);
-    sig_k_K = zeros(n+1,1);
-
+    z_t = zeros(n+1,1); z_t₁ = zeros(n+1,1); z_T = zeros(n+1,1);
+    σ_t = zeros(n+1,1); σ_t₁ = zeros(n+1,1); σ_T = zeros(n+1,1);
     S = zeros(n,1);
 
     # batch state-space model outputs for the two attentional markers
@@ -182,7 +170,7 @@ function attention(x1,x2;
 
     P(x,i) = (1/x)*sqrt(ρ_d[i])*exp(-0.5ρ_d[i]*(log(x)-μ_d[i])^2)
     function outerE(x1,x2,z)
-        p = sig(z)
+        p = 1/(1+exp(-z))
         p*P(x1,1)*P(x2,1) /
             (p*P(x1,1)*P(x2,2) + (1-p)*(P(x1,2)*P(x2,2)))
     end
@@ -214,47 +202,44 @@ function attention(x1,x2;
 
             # filtering
             for t in 2:n+1
-                z_k_k_1[t] = z_k_k[t-1];
-                sig_k_k_1[t] = sig_k_k[t-1] + η[t-1];
+                z_t₁[t] = z_t[t-1];
+                σ_t₁[t] = σ_t[t-1] + η[t-1];
 
                 # Newton's algorithm
                 for m=1:newton_iter
-                    a = z_k_k[t] - z_k_k_1[t] - sig_k_k_1[t]*(E[t-1] -
-                        exp(z_k_k[t])/(1+exp(z_k_k[t])))
-                    b = 1 + sig_k_k_1[t]*exp(z_k_k[t])/((1+exp(z_k_k[t]))^2)
-                    z_k_k[t] -= a / b;
+                    a = z_t[t] - z_t₁[t] - σ_t₁[t]*(E[t-1] -
+                        exp(z_t[t])/(1+exp(z_t[t])))
+                    b = 1 + σ_t₁[t]*exp(z_t[t])/((1+exp(z_t[t]))^2)
+                    z_t[t] -= a / b;
                 end
-                sig_k_k[t] = 1 / (1/sig_k_k_1[t] +
-                    exp(z_k_k[t])/((1+exp(z_k_k[t]))^2));
+                σ_t[t] = 1 / (1/σ_t₁[t] +
+                    exp(z_t[t])/((1+exp(z_t[t]))^2));
             end
 
             # smoothing
-            z_k_K[n+1] = z_k_k[n+1];
-            sig_k_K[n+1] = sig_k_K[n+1];
+            z_T[end] = z_t[n+1];
+            σ_T[n+1] = σ_T[n+1];
 
             for t = reverse(1:n)
-                S[t] = sig_k_k[t]*1/sig_k_k_1[t+1];
-                z_k_K[t] = z_k_k[t] + S[t]*(z_k_K[t+1] - z_k_k_1[t+1])
-                sig_k_K[t] = sig_k_k[t] + S[t]^2*(sig_k_K[t+1] -
-                    sig_k_k_1[t+1]);
+                S[t] = σ_t[t]*1/σ_t₁[t+1];
+                z_T[t] = z_t[t] + S[t]*(z_T[t+1] - z_t₁[t+1])
+                σ_T[t] = σ_t[t] + S[t]^2*(σ_T[t+1] -
+                    σ_t₁[t+1]);
             end
 
-            z_k_k[1] = z_k_K[1]
-            sig_k_k[1] = sig_k_K[1]
+            z_t[1] = z_T[1]
+            σ_t[1] = σ_T[1]
 
             ####################
             # inner M (update z and η)
-            a = (z_k_K[2:end]-z_k_K[1:end-1]).^2 + sig_k_K[2:end] +
-                sig_k_K[1:end-1]-2*sig_k_K[2:end].*S + 2*b₀
-            η = a/(1+2*(a₀+1));
-
+            η = ((z_T[2:end]-z_T[1:end-1]).^2 + σ_T[2:end] +
+                 σ_T[1:end-1]-2*σ_T[2:end].*S + 2*b₀) / (1+2*(a₀+1));
         end
 
         # update the z's
-        z = z_k_K[2:end];
+        z = z_T[2:end];
     end
 
-
 end
 
-end
+end # module EEGAttentionMarker
