367eb785d154181be1f4f8a01f031a2a7ede22fb
David Little <david.frank.little@gmail.com>
2019-05-16 16:01:59 -0400

    improved speed of decoding

--- analyses/test_decode.jl
+++ analyses/test_decode.jl
@@ -13,13 +13,13 @@ close(mf)
 SampledSignals.samplerate(x::MetaArray) = x.samplerate
 eegd = meta(eeg,samplerate=200)
 
-m1,m2 = EEGCoding.marker(eegd,Y1,Y2)
+# m1,m2 = EEGCoding.marker(eegd[1:div(end,4),:],Y1[1:div(end,4),:],Y2[1:div(end,4),:],maxit=1000)
+m1,m2 = EEGCoding.marker(eegd,Y1,Y2,maxit=250,tol=2e-3,progress=false)
+y,yl,yu = EEGCoding.attention(m1,m2)
 
 scene = Scene()
 lines!(scene,axes(m1)[1],clamp.(m1,1,4))
 lines!(scene,axes(m2)[1],clamp.(m2,1,4),color=:blue)
-
-y,yl,yu = EEGCoding.attention(m1,m2)
 
 scene = Scene()
 band!(scene,axes(y)[1],yl,yu)
@@ -27,4 +27,4 @@ lines!(scene,axes(y)[1],y)
 
 
 # TODO: now that the functions are working test it out on our actual data and
-# see if we can generate a good interface
+# see if we can generate a good interface for EEGCoding in the process
