57b548901abf62b99496362f56538cf3bbd8b3cd
haberdashpi <david.frank.little@gmail.com>
2019-08-15 10:20:00 -0400

    Reorg 3: reorganized analyses folder.

---
+++ scripts/julia/install.jl
@@ -0,0 +1,16 @@
+using DrWatson, Pkg
+quickactivate(@__DIR__,"german_track")
+Pkg.instantiate()
+
+using TOML
+
+config = joinpath(projectdir(),"install.toml")
+if !isfile(config)
+    error("Could not find install.toml: see README.md")
+elseif !isdir(datadir())
+    name = TOML.parsefile(config)["data"]
+    s#ymlink(name,datadir()[1:end-1])
+    @info "The folder `data` now links to $name."
+else
+    @info "Directory `data` has already been created."
+end
