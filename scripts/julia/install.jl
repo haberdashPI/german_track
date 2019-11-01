2942606a5f918b2ed8345de99fe10625fd665a3b
David Little <david.frank.little@gmail.com>
2019-11-01 16:24:04 -0400

    fix: install.jl uses correct data path
    Revised due to changes in `DrWatson`

--- scripts/julia/install.jl
+++ scripts/julia/install.jl
@@ -9,7 +9,7 @@ if !isfile(config)
     error("Could not find install.toml: see README.md")
 elseif !isdir(datadir())
     name = TOML.parsefile(config)["data"]
-    symlink(name,datadir()[1:end-1])
+    symlink(name,datadir())
     @info "The folder `data` now links to $name."
 else
     @info "Directory `data` has already been created."
