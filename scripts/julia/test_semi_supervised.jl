dc9d116ded97cafd68d983e6c16984b89034c5a2
haberdashpi <david.frank.little@gmail.com>
2020-02-19 18:05:57 -0500

    testerd larger version of unsupervised problem

--- scripts/julia/test_semi_supervised.jl
+++ scripts/julia/test_semi_supervised.jl
@@ -1,4 +1,5 @@
-using DrWatson; @quickactivate("german_track")
+using DrWatson
+@quickactivate("german_track")
 
 using EEGCoding, SignalOperators, SignalOperators.Units, RCall, Distributions
 Uniform = Distributions.Uniform
@@ -17,7 +18,7 @@ function randweight(n)
     result
 end
 
-
+#=
 envelopes = [reshape(reduce(hcat,randenvelope(5s,22Hz) for h in 1:H),H,1,:)
     for t in 1:T]
 w = reduce(hcat,randweight(3) for t in 1:T)'
@@ -32,18 +33,24 @@ a, ŵ = EEGCoding.regressSS(eeg,envelopes,0.5)
 plotaxes(vec(a))
 plotaxes(ŵ)
 plotaxes(w')
-
+ =#
 # okay, that looks good, let's try something a little larges
 T = 1_000
 H = 3
 
-envelopes = [reshape(reduce(hcat,randenvelope(5s,22Hz) for h in 1:H),H,1,:)
+envelopes = [reshape(reduce(hcat,randenvelope(2s,22Hz) for h in 1:H),H,1,:)
     for t in 1:T]
 w = reduce(hcat,randweight(3) for t in 1:T)'
 
-eeg = [vcat(0.2randn(20,size(envelopes[1],3)),
-            0.2randn(5,size(envelopes[1],3)) .+
+eeg = [vcat(0.2randn(490,size(envelopes[1],3)),
+            0.2randn(10,size(envelopes[1],3)) .+
             sum(envelopes[t][h,:,:] * w[t,h] for h in 1:H))
        for t in 1:T]
 
 a, ŵ = EEGCoding.regressSS(eeg,envelopes,0.5)
+
+using PlotAxes, RCall
+plotaxes(vec(a))
+plotaxes(ŵ)
+R"quartz()"
+plotaxes(w)
