156e2f53f709d30e7b6b9cd3eee85b5c049a9513
haberdashpi <david.frank.little@gmail.com>
2020-05-04 13:54:13 -0400

    add miss only

--- scripts/julia/category_grid.jl
+++ scripts/julia/category_grid.jl
@@ -94,6 +94,66 @@ pl = subj_means |>
 save(File(format"PDF",joinpath(dir,"hitonly_svm_allbins.pdf")),pl)
 
 
+powerdf = @_ freqmeans |>
+    filter(_.condition in [:global,:object],__) |>
+    filter(_.hit == :miss,__) |>
+    stack(__, Between(:delta,:gamma),
+        variable_name = :freqbin, value_name = :power) |>
+    filter(all(!isnan,_.power), __)
+
+ε = max(1e-8,minimum(filter(!iszero,powerdf.power))/2)
+powerdiff_df = @_ powerdf |>
+    unstack(__, :window_timing, :power) |>
+    by(__, [:sid,:hit,:freqbin,:condition,:winstart,:winlen,:channel,:salience],
+        (:before,:after) => sdf ->
+            (powerdiff = mean(log.(ε .+ sdf.after) .- log.(ε .+ sdf.before)),))
+
+powerdiff_df[!,:channel_bin] .=
+    categorical(Symbol.(:channel_,powerdiff_df.channel,:_,powerdiff_df.freqbin))
+
+classdf = @_ powerdiff_df |>
+    unstack(__, [:sid, :condition, :winstart, :winlen, :salience],
+        :channel_bin, :powerdiff) |>
+    filter(all(!ismissing,_[r"channel"]), __) |>
+    disallowmissing!(__,r"channel")
+
+
+# TODO: for some reason we still have infinite values in powerdiff
+# figure that out and then create the plot
+classpredict = by(classdf, [:winstart,:winlen,:salience]) do sdf
+    labels = testmodel(NuSVC(),sdf,:sid,:condition,r"channel")
+    DataFrame(correct = sdf.condition .== labels,sid = sdf.sid)
+end
+
+subj_means = @_ classpredict |>
+    by(__,[:winstart,:winlen,:salience],:correct => mean)
+
+sort!(subj_means,order(:correct_mean,rev=true))
+first(subj_means,6)
+
+dir = joinpath(plotsdir(),string("results_",Date(now())))
+isdir(dir) || mkdir(dir)
+
+subj_means.llen = log.(2,subj_means.winlen)
+subj_means.lstart = log.(2,subj_means.winstart)
+
+pl = subj_means |>
+    @vlplot(:rect,
+        x={
+            field=:lstart,
+            bin={step=4/9,anchor=-3-2/9},
+        },
+        y={
+            field=:llen,
+            bin={step=4/9,anchor=-3-2/9},
+        },
+        color={:correct_mean,scale={reverse=true,domain=[0.5,1],scheme="plasma"}},
+        column=:salience,
+        row={field=:freqbin,type=:ordinal,
+             sort=[:delta,:theta,:alpha,:beta,:gamma]})
+
+save(File(format"PDF",joinpath(dir,"missonly_svm_allbins.pdf")),pl)
+
 freqbins = OrderedDict(
     :delta => (1,3),
     :theta => (3,7),
