f1b8f19718f28be058985a93fc2bc312c943737f
haberdashpi <david.frank.little@gmail.com>
2020-05-08 08:58:58 -0400

    best of both object and spatial

--- scripts/julia/category_grid.jl
+++ scripts/julia/category_grid.jl
@@ -42,39 +42,6 @@ isdir(dir) || mkdir(dir)
 # TODO: try running each window separatley and storing the
 # results, rather than storing all versions of the data
 
-# classdf_file = joinpath(cache_dir(),"data","freqmeans_trial.csv")
-# if isfile(classdf_file)
-#     classdf = CSV.read(classdf_file)
-# else
-#     # NOTE: in principle this could also be run in parallel (doesn't feel worth it
-#     # since FFTW doesn't handle multi-threading yet, so we'd have to setup more
-#     # multi-process parallism)
-
-#     classdf = find_powerdiff(
-#         subjects,groups=[:salience,:target_time,:trial,:sound_index],
-#         hittypes = ["hit"],
-#         winlens = 2.0 .^ range(-1,1,length=10),
-#         winstarts = [0; 2.0 .^ range(-2,1,length=9)])
-#     CSV.write(classdf_file,classdf)
-# end
-
-# classdf_file = joinpath(cache_dir(),"data","freqmeans_chunk20.csv")
-# if isfile(classdf_file)
-#     classdf = CSV.read(classdf_file)
-# else
-#     # NOTE: in principle this could also be run in parallel (doesn't feel worth it
-#     # since FFTW doesn't handle multi-threading yet, so we'd have to setup more
-#     # multi-process parallism)
-
-#     classdf = find_powerdiff(
-#         subjects,groups=[:salience,:target_time,:chunk],
-#         chunk_size = 20,
-#         hittypes = ["hit"],
-#         winlens = 2.0 .^ range(-1,1,length=10),
-#         winstarts = [0; 2.0 .^ range(-2,1,length=9)])
-#     CSV.write(classdf_file,classdf)
-# end
-
 classdf_file = joinpath(cache_dir(),"data","freqmeans_sal.csv")
 if isfile(classdf_file)
     classdf = CSV.read(classdf_file)
@@ -88,11 +55,7 @@ else
 end
 
 @everywhere begin
-    # classdf_file = joinpath(cache_dir(),"data","freqmeans_trial.csv")
-    # classdf_file = joinpath(cache_dir(),"data","freqmeans.csv")
     classdf_file = joinpath(cache_dir(),"data","freqmeans_sal.csv")
-    # classdf_file = joinpath(cache_dir(),"data","freqmeans_chunk.csv")
-    # classdf_file = joinpath(cache_dir(),"data","freqmeans_chunk20.csv")
     classdf = CSV.read(classdf_file)
 
     objectdf = @_ classdf |> filter(_.condition in ["global","object"],__)
@@ -157,10 +120,10 @@ end
 end
 testgroups = @_ objectdf |> filter(_.sid ∉ vset,__) |>
     groupby(__, [:winstart,:winlen,:salience])
-classpredict = dreduce(append!!,Map(x -> modelresult(x,best_params)),
+object_classpredict = dreduce(append!!,Map(x -> modelresult(x,best_params)),
     collect(pairs(testgroups)),init=Empty(DataFrame))
 
-subj_means = @_ classpredict |>
+subj_means = @_ object_classpredict |>
     groupby(__,[:winstart,:winlen,:salience]) |>
     combine(__,:correct => mean)
 
@@ -195,7 +158,7 @@ best_low = @_ subj_means |> filter(_.salience == "low",__) |>
     sort(__,:correct_mean,rev=true) |>
     first(__,1)
 
-best_vals = @_ classpredict |>
+best_vals = @_ object_classpredict |>
     filter((_1.winstart == best_high.winstart[1] &&
             _1.winlen == best_high.winlen[1]) ||
            (_1.winstart == best_low.winstart[1] &&
@@ -231,3 +194,155 @@ pl =
 
 
 save(joinpath(dir, "object_salience_best.pdf"),pl)
+
+@everywhere begin
+    spatialdf = @_ classdf |> filter(_.condition in ["global","spatial"],__)
+end
+
+vset = @_ spatialdf.sid |> unique |>
+    StatsBase.sample(MersenneTwister(111820),__1,round(Int,0.2length(__1)))
+valgroups = @_ spatialdf |> filter(_.sid ∈ vset,__) |>
+    groupby(__, [:winstart,:winlen,:salience])
+
+paramdir = joinpath(datadir(),"svm_params")
+isdir(paramdir) || mkdir(paramdir)
+paramfile = joinpath(paramdir,"subject_salience.csv")
+if !isfile(paramfile)
+    progress = Progress(opts.MaxFuncEvals,"Optimizing params...")
+    best_params, fitness = optparams(param_range;opts...) do params
+        gr = collect(valgroups)
+        correct = dreduce(max,Map(i -> modelacc(valgroups[i],params)),1:length(gr))
+        next!(progress)
+        N = sum(g -> size(g,1),gr)
+        return 1 - correct/N
+    end
+    finish!(progress)
+    best_params = GermanTrack.apply(param_by,best_params)
+
+    CSV.write(paramfile, [best_params])
+else
+    best_params = NamedTuple(first(CSV.read(paramfile)))
+end
+
+@everywhere function modelresult((key,sdf),params)
+    result = testmodel(sdf,NuSVC(;params...),
+        :sid,:condition,r"channel")
+    foreach(kv -> result[!,kv[1]] .= kv[2],pairs(key))
+    result
+end
+testgroups = @_ spatialdf |> filter(_.sid ∉ vset,__) |>
+    groupby(__, [:winstart,:winlen,:salience])
+spatial_classpredict = dreduce(append!!,Map(x -> modelresult(x,best_params)),
+    collect(pairs(testgroups)),init=Empty(DataFrame))
+
+subj_means = @_ spatial_classpredict |>
+    groupby(__,[:winstart,:winlen,:salience]) |>
+    combine(__,:correct => mean)
+
+sort!(subj_means,order(:correct_mean,rev=true))
+first(subj_means,6)
+
+dir = joinpath(plotsdir(),string("results_",Date(now())))
+isdir(dir) || mkdir(dir)
+
+subj_means.llen = log.(2,subj_means.winlen)
+subj_means.lstart = log.(2,subj_means.winstart)
+
+pl = subj_means |>
+    @vlplot(:rect,
+        x={
+            field=:lstart,
+            bin={step=4/9,anchor=-3-2/9},
+        },
+        y={
+            field=:llen,
+            bin={step=4/9,anchor=-3-2/9},
+        },
+        color={:correct_mean,scale={reverse=true,domain=[0.5,1],scheme="plasma"}},
+        column=:salience)
+
+save(joinpath(dir,"spatial_salience.pdf"),pl)
+
+best_high = @_ subj_means |> filter(_.salience == "high",__) |>
+    sort(__,:correct_mean,rev=true) |>
+    first(__,1)
+best_low = @_ subj_means |> filter(_.salience == "low",__) |>
+    sort(__,:correct_mean,rev=true) |>
+    first(__,1)
+
+best_vals = @_ spatial_classpredict |>
+    filter((_1.winstart == best_high.winstart[1] &&
+            _1.winlen == best_high.winlen[1]) ||
+           (_1.winstart == best_low.winstart[1] &&
+            _1.winlen == best_low.winlen[1]),__) |>
+    groupby(__,[:winlen,:winstart,:salience]) |>
+    combine(:correct => function(x)
+        bs = bootstrap(mean,x,BasicSampling(10_000))
+        μ,low,high = 100 .* confint(bs,BasicConfInt(0.683))[1]
+        (correct = μ, low = low, high = high)
+    end,__)
+
+best_vals.winlen .= round.(best_vals.winlen,digits=2)
+best_vals[!,:window] .= (format.("width = {:1.2f}s, start = {:1.2f}s",
+    best_vals.winlen,best_vals.winstart))
+
+pl =
+    @vlplot() +
+    @vlplot(data=[{}], mark=:rule,
+    encoding = {
+      y = {datum = 50},
+      strokeDash = {value = [2,2]}
+    }) +
+    (best_vals |>
+     @vlplot(x={:window, type=:ordinal, axis={title="Window"}}) +
+     @vlplot(mark={:errorbar,filled=true},
+            y={"low",scale={zero=false}, axis={title=""},type=:quantitative},
+            y2={"high", type=:quantitative}, color=:salience) +
+     @vlplot(mark={:point,filled=true},
+            y={:correct,scale={zero=false},axis={title="% Correct Classification"}},
+            color=:salience))
+
+# TODO: add a dotted line to chance level
+
+save(joinpath(dir, "spatial_salience_best.pdf"),pl)
+
+all_subj_means = @_ insertcols!(spatial_classpredict,:comparison => "spatial") |>
+    vcat(__,insertcols!(object_classpredict,:comparison => "object")) |>
+    groupby(__,[:comparison,:winstart,:winlen,:salience]) |>
+    combine(__,:correct => mean => :correct) |>
+    groupby(__,[:winstart,:winlen,:salience]) |>
+    combine(__,:correct => mean => :correct)
+
+best_high = @_ all_subj_means |> filter(_.salience == "high",__) |>
+    sort(__,:correct,rev=true) |>
+    first(__,1)
+best_low = @_ all_subj_means |> filter(_.salience == "low",__) |>
+    sort(__,:correct,rev=true) |>
+    first(__,1)
+
+best_vals = @_ vcat(spatial_classpredict,object_classpredict) |>
+    filter((_1.winstart == best_high.winstart[1] &&
+                _1.winlen == best_high.winlen[1]) ||
+           (_1.winstart == best_low.winstart[1] &&
+            _1.winlen == best_low.winlen[1]),__) |>
+    groupby(__,[:winlen,:winstart,:salience,:comparison]) |>
+    combine(:correct => function(x)
+        bs = bootstrap(mean,x,BasicSampling(10_000))
+        μ,low,high = 100 .* confint(bs,BasicConfInt(0.683))[1]
+        (correct = μ, low = low, high = high)
+    end,__)
+
+best_vals.winlen .= round.(best_vals.winlen,digits=2)
+best_vals[!,:window] .= (format.("width = {:1.2f}s, start = {:1.2f}s",
+    best_vals.winlen,best_vals.winstart))
+
+R"""
+
+library(ggplot2)
+ggplot($best_vals,aes(x=window,y=correct,color=salience,group=salience)) +
+    geom_pointrange(aes(ymin = low, ymax = high),position=position_dodge(width=0.3)) +
+    facet_grid(~comparison) + geom_abline(intercept=50,slope=0,linetype=2) +
+    ylim(50,100)
+ggsave(file.path($dir,"best_object_and_spatial.pdf"))
+
+"""
