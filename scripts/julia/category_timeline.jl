e8812a69f5de52348447053cd1cdc9dd7f08a48f
David Little <david.frank.little@gmail.com>
2020-06-21 11:21:46 -0400

    possible bar graph summary

--- scripts/julia/category_timeline.jl
+++ scripts/julia/category_timeline.jl
@@ -62,7 +62,7 @@ function best_windows_for(df)
     Iterators.product(winstarts,winlens)
 end
 
-classdf_file = joinpath(cache_dir(),"data","freqmeans_timeline_sal_target_time.csv")
+classdf_file = joinpath(cache_dir(),"data","freqmeans_timeline_sal_target_time_withmiss.csv")
 if use_cache && isfile(classdf_file)
     classdf = CSV.read(classdf_file)
 else
@@ -74,8 +74,9 @@ else
     events = @_ mapreduce(_.events,append!!,values(subjects))
     classdf_groups = @_ events |>
         filter(_.target_present,__) |>
-        filter(ishit(_,region = "target") == "hit",__) |> # hits only
-        groupby(__,[:salience_label,:target_time_label,:sid,:condition])
+        insertcols!(__,:hit => ishit.(eachrow(__),region = "target")) |>
+        filter(_.hit ∈ ["hit", "miss"],__) |>
+        groupby(__,[:hit,:salience_label,:target_time_label,:sid,:condition])
 
     progress = Progress(length(classdf_groups),desc="Computing frequency bins...")
     classdf = @_ classdf_groups |>
@@ -152,6 +153,9 @@ predict = vcat(object_predict,spatial_predict)
 dir = joinpath(plotsdir(),string("results_",Date(now())))
 isdir(dir) || mkdir(dir)
 
+# Timeline across salience x target time
+# -----------------------------------------------------------------
+
 band = @_ predict |>
     # filter(_.before == "zero",__) |>
     groupby(__,[:winstart,:salience_label,:target_time_label,:condition]) |> #,:before]) |>
@@ -176,62 +180,119 @@ R"""
 ggsave(file.path($dir,"object_salience_timeline.pdf"),pl,width=11,height=8)
 """
 
-best_wins = @_ predict |>
-    groupby(__,[:winstart,:salience,:condition]) |>
-    combine(__,:correct_mean => mean => :correct) |>
-    groupby(__,[:salience,:condition]) |>
-    combine(__,[:winstart,:correct] =>
-        ((win,val) -> win[argmax(val)]) => :winstart) |>
-    groupby(__,:condition)
+# Timeline across salience
+# -----------------------------------------------------------------
 
-subj_sum = @_ predict |>
-    filter(_.winstart in best_wins[(condition = _.condition,)].winstart, __) |>
-    groupby(__,[:winstart,:salience,:condition]) |>
+band = @_ predict |>
+    # filter(_.before == "zero",__) |>
+    groupby(__,[:winstart,:salience_label,:condition,:sid]) |> #,:before]) |>
+    combine(__,:correct_mean => mean => :correct_mean) |>
+    groupby(__,[:winstart,:salience_label,:condition]) |> #,:before]) |>
     combine(:correct_mean => function(correct)
         bs = bootstrap(mean,correct,BasicSampling(10_000))
         μ,low,high = 100 .* confint(bs,BasicConfInt(0.682))[1]
         (correct = μ, low = low, high = high)
-    end,__)
-
-subj_sum = @_ subj_sum |>
-    groupby(__,[:condition,:winstart]) |>
-    transform!(__,[:condition,:winstart] => function(cond,start)
-        best = best_wins[(condition = cond[1],)]
-        bestfor = filter(x -> x.winstart == start[1],best)
-        fill(bestfor.salience[1], length(start))
-    end => :bestfor)
-
-subj_sum = @_ subj_sum |>
-    groupby(__,[:condition,:winstart,:bestfor]) |>
-    transform!(__,[:winstart,:bestfor] => function(start,bestfor)
-        @show start
-        label = "Best Time \nfor $(uppercasefirst(bestfor[1]))\n"*
-            "($(round(start[1],digits=2)) s)"
-        fill(label,length(start))
-    end => :label)
+    end,__) #|>
+    # transform!(__,[:salience,:before] =>
+    #     ((x,y) -> string.(x,"_",y)) => :salience_for)
 
 R"""
-pl = plot_grid(
-    ggplot(filter($subj_sum,condition == "object"),
-        aes(x=label,y=correct,color=salience,group=salience)) +
-        geom_bar(stat='identity',position=position_dodge(width=0.8),aes(fill=salience),width=0.6) +
-        geom_linerange(aes(ymin=low,ymax=high),position=position_dodge(width=0.8),color='black') +
-        guides(color=F,fill=F) + coord_cartesian(ylim=c(40,100)) +
-        geom_abline(intercept = 50, slope = 0, linetype = 2) +
-        xlab("") + ylab("% Correct"),
-    ggplot(filter($subj_sum,condition == "spatial"),
-        aes(x=label,y=correct,color=salience,group=salience)) +
-        geom_bar(stat='identity',position=position_dodge(width=0.8),aes(fill=salience),width=0.6) +
-        geom_linerange(aes(ymin=low,ymax=high),position=position_dodge(width=0.8),color='black') +
-        coord_cartesian(ylim=c(40,100)) +
-        geom_abline(intercept = 50, slope = 0, linetype = 2) +
-        xlab("") + ylab("% Correct"),
-        rel_widths = c(1,1.2),
-    labels = c('Object', 'Spatial'), label_x=0.5, hjust=0.5)
+pl = ggplot($band,aes(x=winstart,y=correct,color=salience_label)) +
+    geom_ribbon(aes(ymin=low,ymax=high,fill=salience_label,color=NULL),alpha=0.4) +
+    geom_line() + facet_grid(~condition) +
+    geom_abline(slope=0,intercept=50,linetype=2) +
+    coord_cartesian(ylim=c(40,100))
+pl
 """
 
 R"""
-ggsave(file.path($dir,"salience_classify_summary.pdf"),pl,width=8,height=6)
+ggsave(file.path($dir,"object_salience.pdf"),pl,width=11,height=8)
+"""
+
+# Timeline across target time
+# -----------------------------------------------------------------
+
+band = @_ predict |>
+    # filter(_.before == "zero",__) |>
+    groupby(__,[:winstart,:target_time_label,:condition]) |> #,:before]) |>
+    combine(:correct_mean => function(correct)
+        bs = bootstrap(mean,correct,BasicSampling(10_000))
+        μ,low,high = 100 .* confint(bs,BasicConfInt(0.682))[1]
+        (correct = μ, low = low, high = high)
+    end,__) #|>
+    # transform!(__,[:salience,:before] =>
+    #     ((x,y) -> string.(x,"_",y)) => :salience_for)
+
+R"""
+pl = ggplot($band,aes(x=winstart,y=correct,color=target_time_label)) +
+    geom_ribbon(aes(ymin=low,ymax=high,fill=target_time_label,color=NULL),alpha=0.4) +
+    geom_line() + facet_grid(~condition) +
+    geom_abline(slope=0,intercept=50,linetype=2) +
+    coord_cartesian(ylim=c(40,100))
+pl
+"""
+
+R"""
+ggsave(file.path($dir,"object_target_time.pdf"),pl,width=11,height=8)
+"""
+
+# Salience grouped into early/late windowstart
+# -----------------------------------------------------------------
+
+# For now, just group by the latest timepoint that miss shows
+# something interesting (~2 seconds).
+grouped = @_ predict |>
+    filter(_.winstart <= 1.5,__) |>
+    transform!(__,:winstart => (x -> ifelse.(x .< 0.5,"early","late")) => :winstart_label) |>
+    groupby(__,[:winstart_label,:salience_label,:condition,:sid]) |>
+    combine(__,:correct_mean => mean => :correct_mean) |>
+    groupby(__,[:winstart_label,:salience_label,:condition]) |>
+    combine(:correct_mean => function(correct)
+        bs = bootstrap(mean,correct,BasicSampling(10_000))
+        μ,low,high = 100 .* confint(bs,BasicConfInt(0.682))[1]
+        (correct = μ, low = low, high = high)
+    end,__) #|>
+
+R"""
+pos = position_dodge(width=0.75)
+ggplot($grouped,aes(x=winstart_label,y=correct,fill=salience_label)) +
+    geom_bar(stat='identity',aes(fill=salience_label),width=0.6,position=pos) +
+    geom_linerange(aes(ymin=low,ymax=high),position=pos) +
+    facet_wrap(~condition) + coord_cartesian(ylim=c(50,100))
+"""
+
+R"""
+ggsave(file.path($dir,"salience_bar.pdf"),width=11,height=8)
+"""
+
+# Target-timing grouped into early/late windowstart
+# -----------------------------------------------------------------
+
+# For now, just group by the latest timepoint that miss shows
+# something interesting (~2 seconds).
+grouped = @_ predict |>
+    filter(_.winstart <= 1.5,__) |>
+    transform!(__,:winstart => (x -> ifelse.(x .< 0.5,"early","late")) => :winstart_label) |>
+    groupby(__,[:winstart_label,:target_time_label,:condition,:sid]) |>
+    combine(__,:correct_mean => mean => :correct_mean) |>
+    groupby(__,[:winstart_label,:target_time_label,:condition]) |>
+    combine(:correct_mean => function(correct)
+        bs = bootstrap(mean,correct,BasicSampling(10_000))
+        μ,low,high = 100 .* confint(bs,BasicConfInt(0.682))[1]
+        (correct = μ, low = low, high = high)
+    end,__) #|>
+
+
+R"""
+pos = position_dodge(width=0.75)
+ggplot($grouped,aes(x=winstart_label,y=correct,fill=target_time_label)) +
+    geom_bar(stat='identity',aes(fill=target_time_label),width=0.6,position=pos) +
+    geom_linerange(aes(ymin=low,ymax=high),position=pos) +
+    facet_wrap(~condition) + coord_cartesian(ylim=c(50,100))
+"""
+
+R"""
+ggsave(file.path($dir,"targettime_bar.pdf"),width=11,height=8)
 """
 
 # Overall vs Miss
