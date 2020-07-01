49d5755c29d5a931a00c303a9f3e6f4339d095cd
David Little <david.frank.little@gmail.com>
2020-07-01 16:26:44 -0400

    improved installation procedure

--- scripts/julia/install.jl
+++ scripts/julia/install.jl
@@ -4,8 +4,14 @@ pkg"activate"
 pkg"add DrWatson"
 
 using DrWatson
+ENV["R_HOME"] = "*" # use Conda to install R (comment this line out if you want to use your own installation)
+mv("Project.toml","Project.toml.backup")
+cp("Project.toml.install","Project.toml")
 cp("Manifest.toml.local","Manifest.toml")
 quickactivate(@__DIR__,"german_track")
+
+pkg"dev src/julia/GermanTrack"
+pkg"dev src/julia/EEGCoding"
 Pkg.instantiate()
 
 using Conda
