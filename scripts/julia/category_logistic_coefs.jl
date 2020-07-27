aaab2ea6914a3f02f5618872e8e5c5851df6d498
David Little <david.frank.little@gmail.com>
2020-07-26 23:22:13 -0400

    wip: plot of logistic coefs by freqbin/channel

--- scripts/julia/category_logistic_coefs.jl
+++ scripts/julia/category_logistic_coefs.jl
@@ -37,6 +37,9 @@ library(dplyr)
 library(Hmisc)
 """
 
+dir = joinpath(plotsdir(),string("results_",Date(now())))
+isdir(dir) || mkdir(dir)
+
 paramdir = processed_datadir("classifier_params")
 best_windows_file = joinpath(paramdir,savename("best-windows",
     (absolute = use_absolute_features, classifier = classifier), "json"))
@@ -51,6 +54,8 @@ best_params = @_ jsontable(open(JSON3.read, paramfile, "r")[:data]) |> DataFrame
     groupby(__, :fold) |>
     combine(__, (params .=> first .=> params)...) |>
     combine(__, (params .=> mean  .=> params)...)
+# select_params = best_params[1,:]
+select_params = (C = 5.0, )
 
 isdir(processed_datadir("features")) || mkdir(processed_datadir("features"))
 classdf_file = joinpath(processed_datadir("features"), savename("freaqmeans",
@@ -69,7 +74,7 @@ classdf = @_ CSV.File(classdf_file) |> DataFrame! |> filter(_.winstart == 0, __)
 
 function findcoefs(sdf)
     model, result = runclassifier(
-        buildmodel(best_params[1,:], classifier, 2017_09_16),
+        buildmodel(select_params, classifier, 2017_09_16),
         data = sdf, X = r"channel", y = :condition, seed = seed
     )
     DataFrame(
@@ -107,14 +112,41 @@ ggplot($coefs, aes(x = target_time_label, y = correct, fill = salience_label)) +
 
 # let's look a the coefficients
 
+coef_names = names(classdf[:,r"channel"])
+function parsecoef(coef)
+    parsed = match(r"channel_([0-9]+)_([a-z]+)",coef_names[coef])
+    chanstr, freqbin = parsed[1], parsed[2]
+    chan = parse(Int,chanstr)
+    chan, freqbin
+end
+
 coefs_spread = @_ coefs |>
     stack(__, All(r"coef"), [:hit, :salience_label, :target_time_label,:condition],
         variable_name = :coef) |>
-    transform!(__, :coef => ByRow(x -> parse(Int,match(r"coef([0-9]+)", x)[1])) => :coef)
+    transform!(__, :coef => ByRow(x -> parse(Int,match(r"coef([0-9]+)", x)[1])) => :coef) |>
+    transform!(__, :coef => ByRow(x -> parsecoef(x)[1]) => :channel) |>
+    transform!(__, :coef => ByRow(x -> parsecoef(x)[2]) => :freqbin)
+
+
+coefs_spread_pl = @_ coefs_spread |>
+    filter(_.hit == "hit" && _.condition != "object-v-spatial",__)
+
 
 R"""
-ggplot($coefs_spread, aes(x = coef, y = value, color = salience_label)) + geom_line() +
-    facet_wrap(hit~condition+target_time_label)
+df = $coefs_spread
+df$freqbin = factor(df$freqbin, levels = unique(df$freqbin), ordered = T)
+df %>% arrange(freqbin)
+pl = ggplot(df, aes(x = channel, y = value, color = salience_label)) + geom_line() +
+    facet_grid(condition~target_time_label+freqbin)
 """
+
+R"""
+ggsave(file.path($dir,"logistic_coefs.pdf"),pl,width=11,height=8)
+"""
+
+# NOTE:
+# - maybe z-score the features by frequency bin?
+# - use a larger sparsity coefficient?
+# -
 
 # TODO: use runclassifier across the various conditions and then inspect coefficients
