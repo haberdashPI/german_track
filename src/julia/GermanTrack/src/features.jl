3b93d43493e8620a71343ad560ff960b676e56f9
David Little <david.frank.little@gmail.com>
2020-08-19 16:47:11 -0400

    cleanup: removed older analysis scripts/functions

--- src/julia/GermanTrack/src/features.jl
+++ src/julia/GermanTrack/src/features.jl
@@ -146,45 +146,8 @@ const default_freqbins = OrderedDict(
     :gamma => (30, 100)
 )
 
-function compute_powerdiff_features(eeg, data, windowfn, window; kwds...)
-    fs = framerate(eeg)
-
-    freqdf = mapreduce(append!!, [:before, :after]) do timing
-        windows = map(eachrow(data)) do row
-            bounds = timing == :before ?
-                (window.before, window.before + window.len) :
-                (window.start, window.start + window.len)
-            windowfn(eeg[row.trial_index], row, fs, bounds...)
-        end
-        signal = reduce(append!!, windows)
-        weight = sum(!isempty, windows)
-        freqdf = computebands(signal, fs; kwds...)
-        freqdf[!, :window_timing] .= string(timing)
-        freqdf[!, :weight] .= weight
-
-        freqdf
-    end
-
-    if size(freqdf, 1) > 0
-        powerdf = @_ freqdf |>
-            stack(__, Between(:delta, :gamma),
-                variable_name = :freqbin, value_name = :power) |>
-            groupby(__, :channel) |>
-            transform!(__, :weight => minimum => :weight) |>
-            filter(all(!isnan, _.power), __) |>
-            unstack(__, :window_timing, :power)
-
-        ε = 1e-8
-        logdiff(x, y) = log.(ε .+ x) .- log.(ε .+ y)
-        powerdiff = logdiff(powerdf.after, powerdf.before)
-
-        chstr = @_(map(@sprintf("%02d", _), powerdf.channel))
-        features = Symbol.("channel_", chstr, "_", powerdf.freqbin)
-        DataFrame(weight = minimum(powerdf.weight);(features .=> powerdiff)...)
-    else
-        Empty(DataFrame)
-    end
-end
+windowbounds(x::NamedTuple,_) = (x.start, x.start + x.len)
+windowbounds(fn::Function, event) = windowbounds(fn(event), event)
 
 function compute_powerbin_features(eeg, data, windowfn, window; kwds...)
     fs = framerate(eeg)
