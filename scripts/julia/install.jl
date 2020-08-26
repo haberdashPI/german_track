bbb5b6581fe772cbcb7722916c0cae45fa08dcb9
David Little <david.frank.little@gmail.com>
2020-08-26 16:50:00 -0400

    chore: R now uses `renv` to manage its a reproducible environemnt

--- scripts/julia/install.jl
+++ scripts/julia/install.jl
@@ -1,10 +1,7 @@
 # add DrWatson to root environment
-using Pkg
-pkg"activate"
-pkg"add DrWatson"
-
+using Pkg; pkg"add DrWatson"
 using DrWatson
-# ENV["R_HOME"] = "*" # use Conda to install R (comment this line out if you want to use your own installation)
+
 mv("Project.toml","Project.toml.backup")
 cp("Project.toml.install","Project.toml")
 cp("Manifest.toml.local","Manifest.toml")
@@ -13,14 +10,3 @@ quickactivate(@__DIR__,"german_track")
 pkg"dev src/julia/EEGCoding"
 pkg"dev src/julia/GermanTrack"
 Pkg.instantiate()
-
-# using Conda
-using Conda
-Conda.add("r-ggplot2",  channel = "conda-forge")
-Conda.add("r-dplyr",    channel = "conda-forge")
-Conda.add("r-cowplot",  channel = "conda-forge")
-Conda.add("r-hmisc",    channel = "conda-forge")
-Conda.add("r-lsr",      channel = "conda-forge")
-Conda.add("r-multcomp", channel = "conda-forge")
-Conda.add("r-rstanarm", channel = "conda-forge")
-Conda.add("r-knitr",    channel = "conda-forge")
--- src/R/setup.R
+++ src/R/setup.R
@@ -1,14 +1,15 @@
 base_dir = getwd()
+if(basename(base_dir) != 'german_track'){
+    warning(paste('Expected root directory to be named "german_track". Was',
+                  basename(base_dir)))
+}
+
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(stringr)
 library(rjson)
 
-if(basename(base_dir) != 'german_track'){ ## TODO: change to 'german_track'
-    warning(paste('Expected root directory to be named "eeg_atten". Was',
-                  basename(base_dir)))
-}
 analysis_dir = file.path(base_dir,'scripts','R')
 cache_dir = file.path(base_dir,'_research','cache','cache')
 processed_datadir = file.path(base_dir,'data','processed')
