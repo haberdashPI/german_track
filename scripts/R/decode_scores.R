aeea0876c91ef074b07cc7ceafb66b6decea1f7d
David Little <david.frank.little@gmail.com>
2021-01-20 10:21:36 -0500

    wip: improving contrast coding

--- scripts/R/decode_scores.R
+++ scripts/R/decode_scores.R
@@ -15,16 +15,36 @@ fit3 = stan_glmer(score ~ target_window * condition +
     adapt_delta = 0.99, # prevents divergent transitions after warm-up
     data = filter(df, target_window %in% c('athit-hit', 'athit-miss')))
 
+# we can use dummy coding to simplify interpretability: the model prior avoids co-linearity
+ct = function(x) { C(x, contr.treatment(levels(x), contrasts = F), nlevels(x)) }
 fit4 = stan_glmer(score ~ target_window * condition * target_time_label *
     target_switch_label * target_salience +
     (target_window * condition | sid) +
     (target_window * condition | stim_id),
     adapt_delta = 0.99, # prevents divergent transitions after warm-up
-    data = filter(df, target_window %in% c('athit-hit', 'athit-miss')))
+    data = filter(df, target_window %in% c('athit-hit', 'athit-miss')) %>%
+        mutate(
+            condition = ct(condition),
+            target_time_label = ct(target_time_label),
+            target_switch_label = ct(target_switch_label),
+            target_salience = ct(target_salience)
+        )
+    )
 
 means = df %>%
     filter(target_window %in% c('athit-hit', 'athit-miss')) %>%
     group_by(condition, sid, target_window) %>%
     summarize(score = mean(score))
 
-stan_glm(score ~ target_window * condition, data = means)
+fitmean = lm(score ~ target_window * condition,
+    data = mutate(means, condition = ct(condition)))
+
+t.test(score ~ target_window, data = filter(means, condition == 'global'), paired = T)
+t.test(score ~ target_window, data = filter(means, condition == 'object'), paired = T)
+t.test(score ~ target_window, data = filter(means, condition == 'spatial' & sid != 10), paired = T)
+
+condmeans = means %>%
+    group_by(sid, target_window) %>%
+    summarize(score = mean(score))
+
+t.test(score ~ target_window, condmeans, paired = T)
