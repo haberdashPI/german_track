79a8ba36c076a0e6ef76eb816de8c0a9256cf1c4
David Little <david.frank.little@gmail.com>
2021-02-10 15:03:30 -0500

    chore: removed unused window selection function

--- src/julia/GermanTrack/src/analyses.jl
+++ src/julia/GermanTrack/src/analyses.jl
@@ -65,55 +65,6 @@ spread(x,scale,npoints;indices=Colon()) =
     quantile.(Normal(x,scale/2),range(0.05,0.95,length=npoints)[indices])
 
 """
-    select_windows(conditions, subjects)
-
-Select windows of eeg data defined by a bounding function.
-
-## Arugments
-
-- conditions: a dictionary where each key is a named tuple describing the
-specific condition windows will be associated with and each entry is a
-function. When applied to the rows of the event data this function will
-return the range of times in seconds (as a tuple) to be included or
-no_indices (if no data applies for the given case)
-
-- subjects: a dictionary containing the file => data pairs, each data entry
-should contain a named tuple of eeg (for EEGData), event (for data frame of
-events) and sid (a String) entries.
-
-"""
-function select_windows(conditions, subjects)
-    reduce(vcat, select_windows_helper.(collect(conditions), Ref(subjects))) |>
-        categorical!
-end
-
-function select_windows_helper((condition, boundfn), subjects)
-    bounds = Dict((file, i) => bounds
-        for file in keys(subjects)
-        for (i, bounds) in enumerate(boundfn.(eachrow(subjects[file].events))))
-    indices = @_ filter(!isempty(bounds[_]), keys(bounds)) |> collect |> sort!
-
-    if !isempty(indices)
-        mapreduce(vcat, indices) do (file, i)
-            eeg, events = subjects[file]
-            start = bounds[(file, i)][1]
-            ixs = bound_indices(bounds[(file, i)], 256, size(eeg[i], 2))
-
-            # power in relevant frequency bins across all channels and times
-            DataFrame(
-                sid = sidfor(file),
-                trial = i;
-                condition...,
-                window = [view(eeg[i], :, ixs)]
-            )
-        end
-    else
-        DataFrame()
-    end
-end
-
-
-"""
     ishit(row; kwds...)
 
 Correctly interprets a given row of the data as a hit, correct rejection, false positive
