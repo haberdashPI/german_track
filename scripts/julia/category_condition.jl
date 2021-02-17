7b661beaa0f5825622cabfb1de62cdeb542bf2b0
David Little <david.frank.little@gmail.com>
2021-02-17 12:58:09 -0500

    fix: bad dependency

--- scripts/julia/category_condition.jl
+++ scripts/julia/category_condition.jl
@@ -7,7 +7,7 @@ using DrWatson
 using GermanTrack, DataFrames, Statistics, Dates, Underscores, Random, Printf,
     ProgressMeter, VegaLite, FileIO, StatsBase, BangBang, Transducers, Infiltrator, Peaks,
     StatsFuns, Distributions, DSP, DataStructures, Colors, Bootstrap, CSV, EEGCoding,
-    JSON3, DataFramesMeta, Lasso, Indexing
+    JSON3, DataFramesMeta, Lasso
 wmean = GermanTrack.wmean
 n_winlens = 6
 
